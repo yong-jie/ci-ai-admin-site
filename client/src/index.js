2f0d31fe2534feee661d5f53c0d6e7c2f9ea7d99
Sora <viorama@gmail.com>
2018-02-19 17:45:14 +0800

    Add router to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />, // eslint-disable-line react/jsx-filename-extension
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
