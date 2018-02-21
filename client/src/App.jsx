ffde678be42f6d1565c068fee7a08e57a272f0cb
Sora <viorama@gmail.com>
2018-02-21 19:50:09 +0800

    Begin designing temperature taking route in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import TemperatureTaking from './TemperatureTaking';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [
+	{
+	  name: 'Jonathan',
+	  id: '1',
+	},
+	{
+	  name: 'Destin',
+	  id: '2',
+	},
+	{
+	  name: 'Marvin',
+	  id: '3',
+	},
+      ],
+    }
+  }
+  
   render() {
     return (
       <div className="App">
         <header>
-          <p>Header</p>
+	  <nav>
+	    <ul>
+	      <li><Link to='/'>Home</Link></li>
+	      <li><Link to='/temperature'>Temperature Taking</Link></li>
+	    </ul>
+	  </nav>
         </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	<Switch>
+	  <Route exact path='/' render={(props) => (
+	    <Home {...props} />
+	    )}/>
+	<Route path='/temperature' render={(props) => (
+	  <TemperatureTaking {...props} students={this.state.students}/>
+	)}/>
+	</Switch>
       </div>
     );
   }
