883134245de9d47efe34f813bd2b4a44f8a4a080
Sora <viorama@gmail.com>
2018-03-07 01:31:09 +0800

    Add redux wrapped app around a provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <App />
+  ,document.getElementById('root'),
 );
 
 // registerServiceWorker();
