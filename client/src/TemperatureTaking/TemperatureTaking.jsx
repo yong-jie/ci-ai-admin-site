46074a736b7cd9d22d5cf303b0e7f12c16fbf239
yong-jie <viorama@gmail.com>
2018-03-08 01:15:01 +0800

    Begin modification for temperature taking frontend

--- client/src/TemperatureTaking/TemperatureTaking.jsx
+++ client/src/TemperatureTaking/TemperatureTaking.jsx
@@ -1,51 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {
-  Container,
-  Col,
-  Row,
-  ListGroup,
-  ListGroupItem,
-  Media,
-  Table,
-} from 'reactstrap';
 import './TemperatureTaking.css';
-import TemperatureInput from './TemperatureInput';
+import { fetchStudentTemperatures } from './TemperatureActionCreator';
 
-const TemperatureTaking = ({ students, handleOnChange }) => {
-  const mappedTopics = students.map(student => (
-    <ListGroupItem key={student.id}>
-      <Container>
-        <Row>
-          <Col xs={'2'} className={'center-vertically'}>
-            <Media
-              object
-              src={'/public/images/placeholder.png'}
-              className={'display-picture'}
-            />
-          </Col>
-          <Col xs={'4'} className={'center-vertically'}>
-            {student.name}
-          </Col>
-          <Col xs={'6'}>
-            <Table bordered>
-              <TemperatureInput
-                id={student.id}
-                entries={student.entries}
-                handleOnChange={handleOnChange}
-              />
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </ListGroupItem>
-  ));
-  return <ListGroup>{mappedTopics}</ListGroup>;
-};
+class TemperatureTaking extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount = () => {
+    fetchStudentTemperatures();
+    this.setState((prevState, props) => ({
+      students: props.students,
+    }));
+  };
+
+  render = () => (
+    <h1>hi</h1>
+  );
+}
 
 TemperatureTaking.propTypes = {
   students: PropTypes.arrayOf(PropTypes.object).isRequired,
-  handleOnChange: PropTypes.func,
 };
 
-export default TemperatureTaking;
+const mapStateToProps = state => ({
+  students: state.students,
+});
+
+export default connect(mapStateToProps)(TemperatureTaking);
