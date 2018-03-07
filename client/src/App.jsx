1815507b343308313afe38629d3e45e16204b2a7
yong-jie <viorama@gmail.com>
2018-03-08 01:14:40 +0800

    Add reducers and api for temperature taking frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { Navbar, Nav, NavItem, NavLink } from 'reactstrap';
+
+import temperatureReducer from './TemperatureTaking/TemperatureReducer';
+
 import Home from './Home';
 import TemperatureTaking from './TemperatureTaking/TemperatureTaking';
 import StudentInformation from './StudentInformation/StudentInformation';
 import './App.css';
 
+const reducers = combineReducers({
+  temperature: temperatureReducer,
+});
+const store = createStore(reducers);
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      students: [
-        {
-          name: '',
-          id: '1',
-          entries: ['', '', '', '', '', '', '', '', '', ''],
-        },
-      ],
-    };
-    this.handleTemperatureInputChange = this.handleTemperatureInputChange.bind(
-      this
-    );
+    this.state = {};
   }
 
   handleTemperatureInputChange(id, entryIndex, value) {
@@ -36,46 +35,48 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar>
-          <Nav tabs>
-            <NavItem>
-              <NavLink tag={Link} to="/">
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={Link} to="/temperature">
-                Temperature Taking
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={Link} to="/information">
-                Student Information
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-        <div className={'route-body'}>
-          <Switch>
-            <Route exact path="/" render={props => <Home {...props} />} />
-            <Route
-              path="/temperature"
-              render={props => (
-                <TemperatureTaking
-                  {...props}
-                  students={this.state.students}
-                  handleOnChange={this.handleTemperatureInputChange}
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar>
+              <Nav tabs>
+                <NavItem>
+                  <NavLink tag={Link} to="/">
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/temperature">
+                    Temperature Taking
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/information">
+                    Student Information
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Navbar>
+            <div className={'route-body'}>
+              <Switch>
+                <Route exact path="/" render={props => <Home {...props} />} />
+                <Route
+                  path="/temperature"
+                  render={props => (
+                    <TemperatureTaking
+                      {...props}
+                    />
+                  )}
                 />
-              )}
-            />
-            <Route
-              path="/information"
-              render={props => <StudentInformation />}
-            />
-          </Switch>
-        </div>
-      </div>
+                <Route
+                  path="/information"
+                  render={props => <StudentInformation />}
+                />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ client/src/TemperatureTaking/TemperatureActionCreator.js
@@ -0,0 +1,15 @@
+import keyMirror from 'keymirror';
+import { fetchTemperatures } from '../api';
+
+export const TemperatureActions = keyMirror({
+  FETCH_STUDENT_TEMPERATURE: null,
+});
+
+export const fetchStudentTemperatures = async () => {
+  const response = await fetchTemperatures();
+  // TODO: Handle error
+  return {
+    type: TemperatureActions.FETCH_STUDENT_TEMPERATURE,
+    payload: response.body.result,
+  };
+};
