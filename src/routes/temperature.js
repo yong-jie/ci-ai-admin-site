da222d587013938bf20b83264af91ab46e106a61
yong-jie <viorama@gmail.com>
2018-03-08 22:11:21 +0800

    Add authorization assertion in fetch temperature api in backend

--- src/routes/temperature.js
+++ src/routes/temperature.js
@@ -3,9 +3,18 @@ import { success, error } from './resultWrapper';
 import { text } from '../constants';
 import { fetchUserTemperatures } from '../controllers/student';
 
+// Only these users can interact with these APIs.
+const requiredAuthorization = ['Teacher', 'Admin'];
+
 const router = Router();
 
 router.get('/users', async (req, res, next) => {
+  const isAuthorized = req.authenticated
+        && requiredAuthorization.includes(req.authorization);
+  if (!isAuthorized) {
+    return res.status(403).json(error(text.unauthorized));
+  }
+
   let students;
   try {
     students = await fetchUserTemperatures();
