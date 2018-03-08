e7ea42dedb4aec1365a4908496a0ed0c33570ee0
yong-jie <viorama@gmail.com>
2018-03-08 13:34:23 +0800

    Edit student model to reference user objects as parents

--- src/models/student.js
+++ src/models/student.js
@@ -4,6 +4,10 @@ const StudentSchema = Schema({
   username: { type: String, required: true }, // NRIC number
   name: { type: String },
   gender: { type: String, required: true, enum: ['Male', 'Female'] },
+  childOf: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }],
   temperatures: [{
     value: { type: Number },
     time: { type: Date },
