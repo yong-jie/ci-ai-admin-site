ddff2de5754e03607d8a20071da5cbf9ce52ac4c
yong-jie <viorama@gmail.com>
2018-03-08 15:48:56 +0800

    Change name field in student model to be compulsory

--- src/models/student.js
+++ src/models/student.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const StudentSchema = Schema({
   username: { type: String, required: true }, // NRIC number
-  name: { type: String },
+  name: { type: String, required: true },
   gender: { type: String, required: true, enum: ['Male', 'Female'] },
   childOf: [{
     type: Schema.Types.ObjectId,
