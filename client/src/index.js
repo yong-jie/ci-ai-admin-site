c9a214f54a8bb82b3e6c0db93c84533de589b46f
yong-jie <viorama@gmail.com>
2018-03-16 23:00:26 +0800

    Remove registerServiceWorker once and for all

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />
   ,document.getElementById('root'),
 );
-
-// registerServiceWorker();
