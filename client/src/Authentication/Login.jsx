073ceb775ecd85529452d27db4c625c913138903
yong-jie <viorama@gmail.com>
2018-03-17 18:25:06 +0800

    Add minimal JSX for login page

--- client/src/Authentication/Login.jsx
+++ client/src/Authentication/Login.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Label, Row, Col, Input } from 'reactstrap';
 
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render = () => (
-    <h1>Hi</h1>
+    <Form>
+      <FormGroup row>
+        <Label for={'username'} sm={2}>Username</Label>
+        <Col sm={10}>
+          <Input type={'text'} id={'username'} />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for={'password'} sm={2}>Password</Label>
+        <Col sm={10}>
+          <Input type={'password'} id={'password'} />
+        </Col>
+      </FormGroup>
+    </Form>
   );
 }
 
